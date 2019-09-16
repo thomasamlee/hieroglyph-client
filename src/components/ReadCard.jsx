27b446c74ceffba033f2eefdb7c1d21af473dece
Thomas Amlee <thomasamlee@gmail.com>
2019-09-15 21:42:29 -0500

    creating branch

--- src/components/ReadCard.jsx
+++ src/components/ReadCard.jsx
@@ -4,10 +4,10 @@ import './ReadCard.scss';
 import { Card, Button } from 'antd';
 
 export default function ReadCard(props) {
-	const { video } = props;
+	const { video, onClose } = props;
 	return (
 		<Card title={video.title} size='default'>
-			<Button>Close</Button>
+			<Button onClick={() => onClose()}>Close</Button>
 			<p>{video.channelTitle}</p>
 			<p>{video.description}</p>
 			<hr />
