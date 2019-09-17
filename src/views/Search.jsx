4afdddd688caded8b55f2be1b889df26b13c319f
Thomas Amlee <thomasamlee@gmail.com>
2019-09-16 19:21:10 -0500

    getting layout fixed

--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Col, Row, Button, Layout, Menu } from 'antd';
+import { Col, Row, Layout } from 'antd';
 
 import ReadCard from '../components/ReadCard';
 import {
@@ -11,27 +11,31 @@ import {
 	DataSearch
 } from '@appbaseio/reactivesearch';
 
-const { Content, Header, Sider } = Layout;
+const { Content, Header } = Layout;
 
 const { ResultListWrapper } = ReactiveList;
 
 export default function Search() {
 	const [selected, setSelected] = useState(null);
 
-	function resultsListRender({ data }) {
-		console.log(data);
-		const resultListMap = data.map((res) => (
-			<ResultList key={res._id} onClick={() => setSelected(res)}>
-				<ResultList.Content>
-					<ResultList.Title>{res.title}</ResultList.Title>
-					<ResultList.Description>Score: {res._score}</ResultList.Description>
-					<ResultList.Description>
-						Channel: {res.channelTitle}
-					</ResultList.Description>
-				</ResultList.Content>
-			</ResultList>
-		));
-		return <ResultListWrapper>{resultListMap}</ResultListWrapper>;
+	function resultsListRender(params) {
+		console.log(params);
+		const { data } = params;
+
+		const resultListMap = data.map((res) => {
+			return (
+				<ResultList key={res._id} onClick={() => setSelected(res)}>
+					<ResultList.Content>
+						<ResultList.Title>{res.title}</ResultList.Title>
+						<ResultList.Description>Score: {res._score}</ResultList.Description>
+						<ResultList.Description>
+							Channel: {res.channelTitle}
+						</ResultList.Description>
+					</ResultList.Content>
+				</ResultList>
+			);
+		});
+		return <div>{resultListMap}</div>;
 	}
 
 	return (
@@ -39,75 +43,60 @@ export default function Search() {
 			app='hiero-videos'
 			credentials='VDqoTiyCf:5c0efad8-8d1b-4a8e-857a-10e3eb8fe67b'
 		>
-			<Header>Hieroglyph</Header>
 			<Layout>
-				<Sider>
-					<MultiDropdownList
-						title='Category'
-						componentId='Category-List'
-						placeholder='Filter Category'
-						dataField='category.keyword'
-						size={5}
-					/>
-					<MultiDropdownList
-						title='Channel'
-						componentId='Channel-List'
-						placeholder='Channel'
-						dataField='channelTitle.keyword'
-						size={5}
-					/>
-				</Sider>
+				<Header>Hieroglyph</Header>
 				<Content>
 					<Row>
-						<Col span={8} offset={8}>
+						<Col span={8} offset={selected ? 4 : 8}>
+							{/* Filters */}
+							<Row gutter={24}>
+								<Col span={12}>
+									<MultiDropdownList
+										componentId='list-category'
+										placeholder='Category'
+										dataField='category.keyword'
+									/>
+								</Col>
+								<Col span={12}>
+									<MultiDropdownList
+										componentId='list-channel'
+										placeholder='Channel'
+										dataField='channelTitle.keyword'
+									/>
+								</Col>
+							</Row>
+
 							<DataSearch
 								autosuggest={false}
-								componentId='Video-Search'
-								title='Video Search'
-								dataField={['title']}
-								fieldWeights={[1, 1]}
-								fuzziness={1}
-								queryFormat='and'
-							/>
-							<DataSearch
-								autosuggest={false}
-								componentId='Transcript-Search'
-								title='Transcript Search'
-								dataField={['transcript']}
-								fieldWeights={[1, 1]}
-								fuzziness={1}
-								queryFormat='and'
+								componentId='search'
+								dataField={['transcript', 'tags', 'title', 'description']}
+								fieldWeights={[10, 1, 1, 10]}
+								fuzziness={0}
+								highlight={true}
+								highlightField={['transcript', 'tags', 'title', 'description']}
+								placeholder='search transcripts'
+								queryFormat='or'
+								style={{
+									marginBottom: 20
+								}}
 							/>
 							<SelectedFilters showClearAll={true} />
-						</Col>
-					</Row>
-
-					<Row gutter={16}>
-						<Col span={8} offset={selected ? 4 : 8}>
 							<ReactiveList
-								componentId='List'
+								componentId='result'
 								dataField='_score'
-								className='result'
-								loader='Loading Results..'
 								pagination
-								size={5}
+								size={10}
 								react={{
-									and: [
-										'Video-Search',
-										'Transcript-Search',
-										'Category-List',
-										'Channel-List'
-									]
+									and: ['list-channel', 'list-category', 'search']
 								}}
 								render={resultsListRender}
 							/>
 						</Col>
-
-						{selected && (
-							<Col span={8}>
+						<Col span={selected ? 8 : 0} style={{ height: '100%' }}>
+							{selected && (
 								<ReadCard video={selected} onClose={() => setSelected(null)} />
-							</Col>
-						)}
+							)}
+						</Col>
 					</Row>
 				</Content>
 			</Layout>
